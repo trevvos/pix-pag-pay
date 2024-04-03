a96433cda5e8ec7074c8cf7d2367375b728cab62
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-04-03 16:55:13 -0300

    default layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import './App.css'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router></Router>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
